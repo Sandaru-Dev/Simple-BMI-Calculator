347ec2cffdf3d795d35cec1e90a442e32ad3c893
SandaruOne <77701910+Sandaru-Dev@users.noreply.github.com>
2021-10-04 00:35:04 +0000

    comment correctly

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-# 🚨 Don't change the code below 👇
+# Get values from console
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
-
+# convert values to float type
 heightint = float(height)
 weightint = float(weight)
 
+# calculate BMI
 BMI =  weightint / (heightint ** 2)
+
+# print BMI
 print("your BMI is : " + str(BMI))
 
 
